3f9b649b833a7e21970893e9e492b4b60c649c81
Matheus Pissaia <pissaia.matheus03@gmail.com>
2021-06-17 00:36:59 -0300

    Estrutura do formulário

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,7 @@
 import Modal from 'react-modal';
 
+import { Container } from './styles';
+
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -8,10 +10,22 @@ interface NewTransactionModalProps {
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
   return (
     <Modal
-        isOpen={isOpen}
-        onRequestClose={onRequestClose}
-      >
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container>
         <h2>Cadastrar Transação</h2>
-      </Modal>
-  )
+
+        <input placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <input placeholder="Categoria" />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Container>
+    </Modal>
+  );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -55,4 +55,28 @@ export const GlobalStyle = createGlobalStyle`
     opacity: 0.6;
     cursor: not-allowed;
   }
+
+  .react-modal-overlay {
+    background: rgba(0, 0, 0, 0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .react-modal-content {
+    background: var(--background);
+
+    position: relative;
+    width: 100%;
+    max-width: 576px;
+    border-radius: 0.25rem;
+    padding: 3rem;
+  }
 `;
