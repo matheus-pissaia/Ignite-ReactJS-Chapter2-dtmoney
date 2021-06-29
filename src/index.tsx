34255c2fade9216742d1996be16d88eb244cecab
Matheus Pissaia <pissaia.matheus03@gmail.com>
2021-06-28 23:38:26 -0300

    Inserindo Transações na API

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  // O MirageJS possui um 'banco de dados' interno, pra usarmos ele 
+  // passamos a propriedade 'models':
+  models: {
+    // Para criar a primeira tabela, passamos o nome da entidade que queremos salvar:
+    transaction: Model,
+  },
+
   routes() {
     // Prefixo para a chamada das rotas subsequentes (exemplo: meusite.com/api)
     // Todas as rotas com '/api' serão redirecionadas para o MirageJS
@@ -11,16 +18,19 @@ createServer({
 
     // Ao realizar uma chamada do tipo GET, será devolvido o objeto abaixo
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transação 1',
-          category: 'Food',
-          type: 'deposit',
-          amount: 400,
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction');
+    })
+
+    /*
+      Abaixo criamos uma rota do tipo 'post' para recebermos 
+      os dados do nosso formuário de transação:
+    */
+    this.post('transactions', (schema, request) => {
+      // Convertendo os dados para JSON:
+      const data = JSON.parse(request.requestBody);
+
+      // Para salvar no banco de dados, passamos na função 'create' o nosso model e os dados:
+      return schema.create('transaction', data);
     })
   }
 })
